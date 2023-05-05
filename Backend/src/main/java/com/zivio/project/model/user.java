b8a8cd99310ce77db7fb5cd1de7a3df4ce27fb41
Caros04 <jowk04@gmail.com>
2023-05-05 09:26:20 +0700

    add auto generate table

---
+++ Backend/src/main/java/com/zivio/project/model/user.java
@@ -0,0 +1,175 @@
+package com.zivio.project.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.Objects;
+
+@Entity
+@Table(name = "users")
+public class user {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String username;
+    private String password;
+    private String name;
+    private String bio;
+    private String email;
+    private char gender;
+    private String phone;
+
+    public user() {
+    }
+
+    public user(int id, String username, String password, String name, String bio, String email, char gender,
+            String phone) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.name = name;
+        this.bio = bio;
+        this.email = email;
+        this.gender = gender;
+        this.phone = phone;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBio() {
+        return this.bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public char getGender() {
+        return this.gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public String getPhone() {
+        return this.phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public user id(int id) {
+        setId(id);
+        return this;
+    }
+
+    public user username(String username) {
+        setUsername(username);
+        return this;
+    }
+
+    public user password(String password) {
+        setPassword(password);
+        return this;
+    }
+
+    public user name(String name) {
+        setName(name);
+        return this;
+    }
+
+    public user bio(String bio) {
+        setBio(bio);
+        return this;
+    }
+
+    public user email(String email) {
+        setEmail(email);
+        return this;
+    }
+
+    public user gender(char gender) {
+        setGender(gender);
+        return this;
+    }
+
+    public user phone(String phone) {
+        setPhone(phone);
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof user)) {
+            return false;
+        }
+        user user = (user) o;
+        return id == user.id && Objects.equals(username, user.username) && Objects.equals(password, user.password)
+                && Objects.equals(name, user.name) && Objects.equals(bio, user.bio) && Objects.equals(email, user.email)
+                && gender == user.gender && Objects.equals(phone, user.phone);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username, password, name, bio, email, gender, phone);
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                " id='" + getId() + "'" +
+                ", username='" + getUsername() + "'" +
+                ", password='" + getPassword() + "'" +
+                ", name='" + getName() + "'" +
+                ", bio='" + getBio() + "'" +
+                ", email='" + getEmail() + "'" +
+                ", gender='" + getGender() + "'" +
+                ", phone='" + getPhone() + "'" +
+                "}";
+    }
+
+}
